ed8c289e0af9b64d7f0d0cc8c5d403b92fe938f9
Anthony Pahl <aj@AJ-MacBook-Pro.local>
2016-07-17 22:48:38 -0400

    Fixed more routing issues with root

--- client/router.js
+++ client/router.js
@@ -1,9 +1,13 @@
 Router.map(function(){
-  this.route('home', {path:'/'});
-	this.route('home', {path:'/home'});
-	this.route('about', {path:'/about'});
-	this.route('contact', {path:'/contact'});
-  this.route('myqueue', {path:'/myqueue'});
-	this.route('search', {path:'/search'});
-	this.route('serverInfo', {path:'/serverInfo'});
+	this.route('home', {path:'home'});	
+	this.route('about', {path:'about'});
+	this.route('contact', {path:'contact'});
+  this.route('myqueue', {path:'myqueue'});
+	this.route('search', {path:'search'});
+	this.route('serverInfo', {path:'serverInfo'});
 })
+
+
+Router.route('/', function () {
+  this.render('home');
+});
