6531ab729be751bc523218dad0349a39f89529bc
AJ Pahl <ajpahl1008@gmail.com>
2016-09-18 20:48:19 -0400

    Added Manual Entry for Application information

--- client/entry/entry.js
+++ client/entry/entry.js
@@ -5,7 +5,7 @@ Template.entry.helpers({
         return Session.get('serverEntry');
     },
     uaid_entry: function() {
-        return Session.get('uaidEntry');
+        return Session.get('applicationEntry');
     },
     owner_entry: function() {
         return Session.get('ownerEntry');
@@ -24,22 +24,22 @@ Template.entry.events({
         Session.set('ownerEntry', false);
         Session.set('patchEntry', false);
     },
-    'change #uaidRadioButton': function () {
+    'change #applicationRadioButton': function () {
         Session.set('serverEntry', false);
-        Session.set('uaidEntry', true);
+        Session.set('applicationEntry', true);
         Session.set('ownerEntry', false);
         Session.set('patchEntry', false);
     },
     'change #ownerRadioButton': function () {
         Session.set('serverEntry', false);
-        Session.set('uaidEntry', false);
+        Session.set('applicationEntry', false);
         Session.set('ownerEntry', true);
         Session.set('patchEntry', false);
 
     },
     'change #patchRadioButton': function () {
         Session.set('serverEntry', false);
-        Session.set('uaidEntry', false);
+        Session.set('applicationEntry', false);
         Session.set('ownerEntry', false);
         Session.set('patchEntry', true);
     },
@@ -70,10 +70,84 @@ Template.entry.events({
 
     },
 
+    'click #serverEntryButton': function (e, template) {
+        var id = template.find('.serverName').value;
+        var operatingSystem = template.find('.osName').value;
+        var operatingSystemVersion = template.find('.osVersion').value;
+        var dateFormat = require('dateformat');
+        var today = dateFormat(today,"yyyy-mm-dd");
+        var lastUpdateDate = today;
+        var relatedUaids = template.find('.relatedApps').value;
+        var serverOwnerId = template.find('.serverOwner').value;
+        var serverInfo = {id, operatingSystem, operatingSystemVersion, lastUpdateDate, relatedUaids, serverOwnerId};
+        if ( id == '' || operatingSystem == '' || operatingSystemVersion =='' || relatedUaids == '' || serverOwnerId == '') {
+            sAlert.warning("Incomplete Data");
+            return;
+        }
+        Meteor.call('addServer', serverInfo, function (error, results) {
+            if (results.data.success != '') {
+                Session.set('savedServer', results.data);
+                console.log(results.data);
+                sAlert.success(results.data.success);
+                clearServerUpdateEntry(e, template);
+            }
+            if (results.data.error != '') {
+                sAlert.error(results.data.error);
+            }
+        });
+
+    },
+    'click #clearServerEntry': function (e, template) {
+        clearServerUpdateEntry(e, template);
+
+    },
+
+    'click #applicationEntryButton': function (e, template) {
+        var id = template.find('.uaidId').value;
+        var name = template.find('.applicationName').value;
+        var description = template.find('.applicationDescription').value;
+        var ownerId = template.find('.applicationOwner').value;
+        var applicationInfo = {id, name, description, ownerId,};
+        if ( id == '' || name == '' || description =='' || ownerId == '' || applicationInfo == '') {
+            sAlert.warning("Incomplete Data");
+            return;
+        }
+        Meteor.call('addApplication', applicationInfo, function (error, results) {
+            if (results.data.success != '') {
+                Session.set('savedApplication', results.data);
+                console.log(results.data);
+                sAlert.success(results.data.success);
+                clearApplicationUpdateEntry(e, template);
+            }
+            if (results.data.error != '') {
+                sAlert.error(results.data.error);
+            }
+        });
+
+    },
+    'click #clearApplicationEntry': function (e, template) {
+        clearApplicationUpdateEntry(e, template);
+
+    },
+
+
+
 });
 
  clearPatchUpdateEntry = function(e, template){
      template.find('form.ui.form').reset();
      template.find('form.ui.form').reset();
+
+}
+
+clearServerUpdateEntry = function(e, template){
+    template.find('form.ui.form').reset();
+    template.find('form.ui.form').reset();
+
+}
+
+clearApplicationUpdateEntry = function(e, template){
+    template.find('form.ui.form').reset();
+    template.find('form.ui.form').reset();
 
 }
--- server/main.js
+++ server/main.js
@@ -3,70 +3,90 @@ var URL_PREFIX;
 var modeAnnouncement;
 
 Meteor.startup(() => {
-     if (process.env.MODE == "DEV") {
-         URL_PREFIX="http://localhost:9080";
-         modeAnnouncement = 'Starting Meteor App - Booster - ' + process.env.MODE;
-     } else if (process.env.MODE == "") {
-         URL_PREFIX="https://booster.pahlsoft.com:9443";
-         modeAnnouncement = 'Starting Meteor App - Booster - PROD';
-     } else {
-         URL_PREFIX="https://booster.pahlsoft.com:9443";
-         modeAnnouncement = 'Starting Meteor App - Booster - PROD';
-     }
-         console.log(modeAnnouncement);
+    if (process.env.MODE == "DEV") {
+        URL_PREFIX = "http://localhost:9080";
+        modeAnnouncement = 'Starting Meteor App - Booster - ' + process.env.MODE;
+    } else if (process.env.MODE == "") {
+        URL_PREFIX = "https://booster.pahlsoft.com:9443";
+        modeAnnouncement = 'Starting Meteor App - Booster - PROD';
+    } else {
+        URL_PREFIX = "https://booster.pahlsoft.com:9443";
+        modeAnnouncement = 'Starting Meteor App - Booster - PROD';
+    }
+    console.log(modeAnnouncement);
 
 });
 
 Meteor.methods({
-          getEntirePatchingList: function() {
-              this.unblock();
-              return Meteor.http.call("GET", URL_PREFIX + '/booster-services/retrieve/booster/getAll');
-          },
-          getEntirePatchingListByOwner: function(owner) {
-              this.unblock();
-              return Meteor.http.call("GET", URL_PREFIX + '/booster-services/retrieve/booster/getBoosterByOwners/' + owner);
-          },
-          getEntirePatchingListByServerName: function(serverName) {
-              this.unblock();
-              return Meteor.http.call("GET", URL_PREFIX + '/booster-services/retrieve/booster/getBoosterByServername/' + serverName);
-          },
-         getEntirePatchingListByUaid: function(uaId) {
-              this.unblock();
-              return Meteor.http.call("GET", URL_PREFIX + '/booster-services/retrieve/booster/getBoosterByUaid/' + uaId);
-          },
-        getEntirePatchingListByPatchId: function(patchId) {
-            this.unblock();
-            return Meteor.http.call("GET", URL_PREFIX + '/booster-services/retrieve/booster/getBoosterByPatchId/' + patchId);
-        },
-          getOwnerList: function() {
-            this.unblock();
-            return Meteor.http.call("GET", URL_PREFIX + '/booster-services/retrieve/owners/getAll');
-          },
-          getUaidList: function() {
-            this.unblock();
-            return Meteor.http.call("GET", URL_PREFIX + '/booster-services/retrieve/uaids/getAll');
-          },
-          getPatchList: function() {
-            this.unblock();
-            return Meteor.http.call("GET", URL_PREFIX + '/booster-services/retrieve/patches/getAll');
-          },
-          getPatchById: function(patchId) {
-            this.unblock();
-            return Meteor.http.call("GET", URL_PREFIX + '/booster-services/retrieve/patches/getPatchById' + patchId);
-          },
-          getPatchByPlatform: function(platform) {
-            this.unblock();
-            return Meteor.http.call("GET", URL_PREFIX + '/booster-services/retrieve/patches/getPatchesByPlatform' + platform);
-          },
-          addPatch: function(patchInfo) {
-              this.unblock();
-              return Meteor.http.call("POST", URL_PREFIX + '/booster-services/update/patches/addPatch' , {
-                  data: patchInfo,
-                  headers: {
-                      "content-type":"application/json",
-                      "Accept":"application/json"
-                  }
-              });
-           },
+    getEntirePatchingList: function () {
+        this.unblock();
+        return Meteor.http.call("GET", URL_PREFIX + '/booster-services/retrieve/booster/getAll');
+    },
+    getEntirePatchingListByOwner: function (owner) {
+        this.unblock();
+        return Meteor.http.call("GET", URL_PREFIX + '/booster-services/retrieve/booster/getBoosterByOwners/' + owner);
+    },
+    getEntirePatchingListByServerName: function (serverName) {
+        this.unblock();
+        return Meteor.http.call("GET", URL_PREFIX + '/booster-services/retrieve/booster/getBoosterByServername/' + serverName);
+    },
+    getEntirePatchingListByUaid: function (uaId) {
+        this.unblock();
+        return Meteor.http.call("GET", URL_PREFIX + '/booster-services/retrieve/booster/getBoosterByUaid/' + uaId);
+    },
+    getEntirePatchingListByPatchId: function (patchId) {
+        this.unblock();
+        return Meteor.http.call("GET", URL_PREFIX + '/booster-services/retrieve/booster/getBoosterByPatchId/' + patchId);
+    },
+    getOwnerList: function () {
+        this.unblock();
+        return Meteor.http.call("GET", URL_PREFIX + '/booster-services/retrieve/owners/getAll');
+    },
+    getUaidList: function () {
+        this.unblock();
+        return Meteor.http.call("GET", URL_PREFIX + '/booster-services/retrieve/uaids/getAll');
+    },
+    getPatchList: function () {
+        this.unblock();
+        return Meteor.http.call("GET", URL_PREFIX + '/booster-services/retrieve/patches/getAll');
+    },
+    getPatchById: function (patchId) {
+        this.unblock();
+        return Meteor.http.call("GET", URL_PREFIX + '/booster-services/retrieve/patches/getPatchById' + patchId);
+    },
+    getPatchByPlatform: function (platform) {
+        this.unblock();
+        return Meteor.http.call("GET", URL_PREFIX + '/booster-services/retrieve/patches/getPatchesByPlatform' + platform);
+    },
+    addPatch: function (patchInfo) {
+        this.unblock();
+        return Meteor.http.call("POST", URL_PREFIX + '/booster-services/update/patches/addPatch', {
+            data: patchInfo,
+            headers: {
+                "content-type": "application/json",
+                "Accept": "application/json"
+            }
+        });
+    },
+    addServer: function (serverInfo) {
+        this.unblock();
+        return Meteor.http.call("POST", URL_PREFIX + '/booster-services/update/inventory/addServer', {
+            data: serverInfo,
+            headers: {
+                "content-type": "application/json",
+                "Accept": "application/json"
+            }
+        });
+    },
+    addApplication: function (applicationInfo) {
+        this.unblock();
+        return Meteor.http.call("POST", URL_PREFIX + '/booster-services/update/applications/addApplication', {
+            data: applicationInfo,
+            headers: {
+                "content-type": "application/json",
+                "Accept": "application/json"
+            }
+        });
+    },
 
-  });
+});
